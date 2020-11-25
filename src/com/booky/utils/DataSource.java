163a51735e3691a8e8a9220adb8fa5603f74a614
dreevo <sidedark85@gmail.com>
2020-11-25 12:33:41 +0100

    Added an empty java project

---
+++ src/com/booky/utils/DataSource.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.booky.utils;
+import java.sql.Connection;
+
+/**
+ *
+ * @author 21655
+ */
+public class DataSource {
+    
+}
